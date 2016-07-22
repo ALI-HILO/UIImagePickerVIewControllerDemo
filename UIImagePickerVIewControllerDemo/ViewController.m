bae8c08f70ac3a54faa9b140a54dea54af2a913c
zhangfogao <fogaozhang@fogaodemacbook-pro.local>
2016-07-23 02:42:50 +0800

    Initial Commit

---
+++ UIImagePickerVIewControllerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIImagePickerVIewControllerDemo
+//
+//  Created by Fogao Zhang on 16/7/23.
+//  Copyright © 2016年 Fogao Zhang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
